6c1524baed52a37843398ae3c1a0963a6e36e00f
chris.hou <yutao@yutao-mbp.local>
2015-02-25 18:26:36 -0800

    init

---
+++ src/sjsu_cs146/PA1.java
@@ -0,0 +1,27 @@
+package sjsu_cs146;
+
+public class PA1 {
+
+    public static void main(String[] args) {
+
+    }
+
+    static class Int {
+
+    }
+
+    static class Out {
+
+    }
+
+    static public void readFromFile() {
+
+    }
+
+    static public void output() {
+
+    }
+
+
+}
+
