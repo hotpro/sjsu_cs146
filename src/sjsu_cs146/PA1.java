112ad50f835f9f07718626cbafb5db447aa7e420
chris.hou <chrishou118@gmail.com>
2015-02-27 23:51:14 -0800

    final version

--- src/sjsu_cs146/PA1.java
+++ src/sjsu_cs146/PA1.java
@@ -1,47 +1,92 @@
 package sjsu_cs146;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 public class PA1 {
-	private static final String INPUT_FILE_NAME = "pa1input2.txt";
-	private static final String OUT_FILE_NAME = "pa1output.txt";
+    private static final String INPUT_FILE_NAME = "pa1input.txt";
+    private static final String OUT_FILE_NAME = "pa1output.txt";
 
     public static void main(String[] args) {
-    	readFromFile();
-        printList(transactions);
-    	basisAndGain();
-    	output();
+        System.out.println("Worker Start");
 
+        List<Transaction> transactions = readFromFile();
+//        printList(transactions);
+
+        Inventory inventory = new Inventory();
+        try {
+            for (Transaction transaction : transactions) {
+                inventory.handleTransaction(transaction);
+            }
+            List<Transaction> current = inventory.getCurrent();
+            List<CostBasisAndGain> costBasisAndGains = inventory.getCostBasisAndGainArrayList();
+
+            FileWriter fileWriter = null;
+            try {
+                fileWriter = new FileWriter(OUT_FILE_NAME);
+                BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+                bufferedWriter.write("Table 1: Current Inventory");
+                bufferedWriter.newLine();
+//                bufferedWriter.write("Serial    #   Item    Quantity    Cost");
+                bufferedWriter.write("Serial # Item Quantity Cost");
+                writeToFile(bufferedWriter, current);
+
+                bufferedWriter.newLine();
+                bufferedWriter.newLine();
+                bufferedWriter.write("Table 2: Cost Basis and Gains");
+                bufferedWriter.newLine();
+                bufferedWriter.write("Serial # Item Quantity Cost Basis ($) Gains ($)");
+                writeToFile(bufferedWriter, costBasisAndGains);
+
+                bufferedWriter.flush();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (fileWriter != null) {
+                    try {
+                        fileWriter.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+//            System.out.println("Table 1: Current Inventory");
+//            printList(current);
+//            System.out.println("Table 2: Cost Basis and Gains");
+//            printList(costBasisAndGains);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Worker End");
     }
     
-    static abstract class Trans {
-    	int serial;
-    	String item;
-    	int q;
-    	int price;
-    	
-    	public Trans(int serial, String item, int q, int price) {
-			super();
-			this.serial = serial;
-			this.item = item;
-			this.q = q;
-			this.price = price;
-		}
-		abstract boolean isIn();
-    	int getSerial() {
-    		return serial;
-    	}
-    	int getQ() {
-    		return q;
-    	}
-    	int getPrice() {
-    		return price;
-    	}
+    static abstract class Transaction {
+        int serial;
+        String item;
+        int quantity;
+        int price;
+        
+        public Transaction(int serial, String item, int quantity, int price) {
+            super();
+            this.serial = serial;
+            this.item = item;
+            this.quantity = quantity;
+            this.price = price;
+        }
+        abstract boolean isIn();
+        int getSerial() {
+            return serial;
+        }
+        int getQuantity() {
+            return quantity;
+        }
+        int getPrice() {
+            return price;
+        }
 
         public String getItem() {
             return item;
@@ -49,171 +94,207 @@ public class PA1 {
 
         @Override
         public String toString() {
-            return "Trans{" +
-                    "serial=" + serial +
-                    ", item='" + item + '\'' +
-                    ", q=" + q +
-                    ", price=" + price +
-                    '}';
+            return serial +
+                    "   " + item +
+                    "   " + quantity +
+                    "   " + price;
         }
     }
 
-    static class In extends Trans {
+    static class InTransaction extends Transaction {
 
-		public In(int serial, String item, int q, int price) {
-			super(serial, item, q, price);
-		}
-
-		@Override
-		public boolean isIn() {
-			
-			return true;
-		}
+        public InTransaction(int serial, String item, int q, int price) {
+            super(serial, item, q, price);
+        }
 
         @Override
-        public String toString() {
-            return "In{}  " + super.toString();
+        public boolean isIn() {
+            
+            return true;
         }
     }
 
-    static class Out extends Trans{
+    static class OutTransaction extends Transaction {
 
-		public Out(int serial, String item, int q, int price) {
-			super(serial, item, q, price);
-		}
-
-		@Override
-		public boolean isIn() {
-			
-			return false;
-		}
+        public OutTransaction(int serial, String item, int quantity, int price) {
+            super(serial, item, quantity, price);
+        }
 
         @Override
-        public String toString() {
-            return "Out{} " + super.toString();
+        public boolean isIn() {
+            
+            return false;
         }
     }
-    
-    static ArrayList<Trans> transactions = new ArrayList<Trans>();
 
-    static void readFromFile() {
 
-    	FileReader fileReader = null;
+    static class Inventory {
+        List<Transaction> current = new ArrayList<Transaction>();
+        List<Transaction> allTransactions = new ArrayList<Transaction>();
+        List<CostBasisAndGain> costBasisAndGainArrayList = new ArrayList<CostBasisAndGain>();
+        Map<String, Integer> counterMap = new HashMap<String, Integer>();
 
-    	try {
-			File file = new File(INPUT_FILE_NAME);
-			fileReader = new FileReader(file);
-			BufferedReader bufferedReader = new BufferedReader(fileReader);
-			StringBuilder sb = new StringBuilder();
-			String line = null;
-            int i = 0;
-			while ((line = bufferedReader.readLine()) != null) {
-				String[] transes = line.split(" +");
-				Trans trans;
-				if ("in".equals(transes[0])) {
-					trans = new In(i, transes[1], Integer.parseInt(transes[2]),
-                            Integer.parseInt(transes[4].substring(1)));
-				} else {
-                    trans = new Out(i, transes[1], Integer.parseInt(transes[2]),
-                            Integer.parseInt(transes[4].substring(1)));
-
-				}
-				transactions.add(trans);
-                i++;
-				
-				sb.append(line);
-				sb.append("\n");
-			}
-			fileReader.close();
-			System.out.println("Contents of file:");
-//			System.out.println(sb.toString());
-
-		} catch (IOException e) {
-			e.printStackTrace();
-			if (fileReader != null) {
-				try {
-					fileReader.close();
-				} catch (IOException e1) {
-					e1.printStackTrace();
-				}
-			}
-		}
-
-    }
-    
-    static void basisAndGain() {
-        int total = 0;
-        ArrayList<Trans> current = new ArrayList<Trans>();
-        ArrayList<CostBasisAndGain> costBasisAndGainArrayList = new ArrayList<CostBasisAndGain>();
-        for (int i = 0; i < transactions.size(); i++) {
-            Trans trans = transactions.get(i);
-            if (trans.isIn()) {
-                current.add(trans);
-                total += trans.getQ();
+        public void handleTransaction(Transaction transaction) throws Exception {
+            if (transaction.isIn()) {
+                in(transaction);
             } else {
-                int outQuantity = trans.getQ();
-                if (outQuantity > total) {
-                    System.out.println("error!!!");
-                } else {
+                out(transaction);
+            }
+        }
 
-                    int p = 0;
-                    int costBasis = 0;
-                    int sumOut = 0;
-                    for (int j = 0; j < i; j++) {
-                        Trans t1 = transactions.get(j);
-                        if (t1.isIn()) {
-                            costBasis += t1.getQ() * t1.getPrice();
+        private void in(Transaction transaction) {
+            if (counterMap.containsKey(transaction.getItem())) {
+                int total = counterMap.get(transaction.getItem());
+                total += transaction.getQuantity();
+                counterMap.put(transaction.getItem(), total);
+            } else {
+                counterMap.put(transaction.getItem(), transaction.getQuantity());
+            }
+
+            current.add(transaction);
+            allTransactions.add(transaction);
+        }
+
+        private void out(Transaction transaction) throws Exception {
+            int total = 0;
+            if (counterMap.containsKey(transaction.getItem())) {
+                total = counterMap.get(transaction.getItem());
+            }
+
+            if (total == 0) {
+                throw new Exception("no inventory");
+            }
+
+            int outQuantity = transaction.getQuantity();
+            if (outQuantity > total) {
+//                System.out.println("error!!!");
+                throw new Exception("out quantity exceeds the inventory");
+            } else {
+
+                int allTransactionSize = allTransactions.size();
+                int position = 0;
+                int costBasis = 0;
+                int sumOfOut = 0;
+                for (int j = 0; j < allTransactionSize; j++) {
+                    Transaction t = allTransactions.get(j);
+                    if (t.getItem().equals(transaction.getItem())) {
+                        if (t.isIn()) {
+                            costBasis += t.getQuantity() * t.getPrice();
                         } else {
-                            sumOut += t1.getQ() * t1.getPrice();
+                            sumOfOut += t.getQuantity() * t.getPrice();
                         }
                     }
-                    sumOut += outQuantity * trans.getPrice();
+                }
+                sumOfOut += outQuantity * transaction.getPrice();
 
-                    total -= outQuantity;
+                CostBasisAndGain costBasisAndGain = new CostBasisAndGain(transaction.getSerial(),
+                        transaction.getItem(), transaction.getQuantity(), costBasis, sumOfOut - costBasis);
+                costBasisAndGainArrayList.add(costBasisAndGain);
 
-                    CostBasisAndGain costBasisAndGain = new CostBasisAndGain(trans.getSerial(),
-                            trans.getItem(), trans.getQ(), costBasis, sumOut - costBasis );
-                    costBasisAndGainArrayList.add(costBasisAndGain);
+                total -= outQuantity;
+                counterMap.put(transaction.getItem(), total);
 
-
-                    if (total == 0) {
-                        // sold out
-                        current.clear();
-                    } else {
-                        for (int j = 0; j < current.size(); j++) {
-                            Trans c = current.get(j);
-                            if (outQuantity >= c.getQ()) {
-                                outQuantity -= c.getQ();
+                if (total == 0) {
+                    // sold out
+                    Iterator<Transaction> iterator = current.iterator();
+                    while (iterator.hasNext()) {
+                        Transaction next = iterator.next();
+                        if (next.getItem().equals(transaction.getItem())) {
+                            iterator.remove();
+                        }
+                    }
+                } else {
+                    List<Transaction> itemsToRemove = new ArrayList<Transaction>();
+                    for (int j = 0; j < current.size(); j++) {
+                        Transaction t = current.get(j);
+                        if (t.getItem().equals(transaction.getItem())) {
+                            if (outQuantity >= t.getQuantity()) {
+                                outQuantity -= t.getQuantity();
+                                itemsToRemove.add(t);
+                            } else if (outQuantity == 0) {
+                                break;
                             } else {
-                                p = j;
+                                current.add(j, new InTransaction(t.getSerial(), t.getItem(), t.getQuantity() - outQuantity,
+                                        t.getPrice()));
+                                itemsToRemove.add(t);
                                 break;
                             }
                         }
-
-                        // remove sold items
-
-                        for (int j = 0; j < p; j++) {
-                            current.remove(0);
-                        }
-
-                        Trans transp = current.get(0);
-                        current.remove(0);
-                        current.add(new In(transp.getSerial(), transp.getItem(), transp.getQ() - outQuantity,
-                                transp.getPrice()));
-
                     }
+
+                    // remove sold items
+                    for (Transaction t : itemsToRemove)
+                        current.remove(t);
                 }
-
-                System.out.println("Table 1: Current Inventory");
-                printList(current);
-                System.out.println("Table 2: Cost Basis and Gains");
-                printList(costBasisAndGainArrayList);
-
+                allTransactions.add(transaction);
             }
         }
 
 
+
+
+        public List<Transaction> getCurrent() {
+            return current;
+        }
+
+        public List<CostBasisAndGain> getCostBasisAndGainArrayList() {
+            return costBasisAndGainArrayList;
+        }
     }
+
+    static List<Transaction> readFromFile() {
+        List<Transaction> transactions = new ArrayList<Transaction>();
+
+        FileReader fileReader = null;
+
+        try {
+            File file = new File(INPUT_FILE_NAME);
+            fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            int i = 0;
+            while ((line = bufferedReader.readLine()) != null && line.length() > 0) {
+                String[] items = line.split(" +");
+                Transaction transaction;
+                int serial = i + 1;
+                String item = items[1].substring(0, items[1].length() - 1);
+                int quantity = Integer.parseInt(items[2]);
+                int price = Integer.parseInt(items[4].substring(1));
+
+                if ("in".equals(items[0])) {
+                    transaction = new InTransaction(serial, item, quantity, price);
+                } else {
+                    transaction = new OutTransaction(serial, item, quantity, price);
+
+                }
+                transactions.add(transaction);
+                i++;
+                
+                sb.append(line);
+                sb.append("\n");
+            }
+            fileReader.close();
+//          System.out.println("Contents of file:");
+//          System.out.println(sb.toString());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fileReader != null) {
+                try {
+                    fileReader.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
+
+        return transactions;
+
+    }
+    
+
 
     static class CostBasisAndGain {
         int serial;
@@ -232,19 +313,19 @@ public class PA1 {
 
         @Override
         public String toString() {
-            return "CostBasisAndGain{" +
-                    "serial=" + serial +
-                    ", item='" + item + '\'' +
-                    ", quantity=" + quantity +
-                    ", costBasis=" + costBasis +
-                    ", gains=" + gains +
-                    '}';
+            return serial +
+                    "   " + item +
+                    "   " + quantity +
+                    "   " + costBasis +
+                    "   " + gains;
         }
     }
 
-
-    static void output() {
-
+    static void writeToFile(BufferedWriter bufferedWriter, List<?> list) throws IOException {
+        for (Object o : list) {
+            bufferedWriter.newLine();
+            bufferedWriter.write(o.toString());
+        }
     }
 
     static void printList(List<?> list) {
@@ -252,7 +333,5 @@ public class PA1 {
             System.out.println(o.toString());
         }
     }
-
-
 }
 
