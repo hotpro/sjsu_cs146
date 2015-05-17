d26ba924b3696b721d76e848a4f7e39cf6e8f3d2
chris.hou <chrishou118@gmail.com>
2015-05-17 15:11:54 -0700

    KruskalMST

--- src/PA4/KruskalMST.java
+++ src/PA4/KruskalMST.java
@@ -31,7 +31,6 @@ public class KruskalMST {
     }
 
     /**
-     * 给定一个带权值的邻接矩阵（其中若i=j，权值为0，若i和j不可达权值为Double.max），返回一个最小生成树，
      *
      * @param graph
      * @return
@@ -56,7 +55,6 @@ public class KruskalMST {
         }
 
         /**
-         * map用于标识边的某个顶点属于哪个集合，认为顶点刚开始属于不同的集合，当选择一条边时，就合并两个集合，如果选择的边在同一个集合内，就代表有环路出现了
          */
         Map<Integer, Set<Integer>> map = new HashMap<Integer, Set<Integer>>();
         int edgeCount = 0;
@@ -67,21 +65,20 @@ public class KruskalMST {
             Set<Integer> setU = map.get(e.u);
             Set<Integer> setV = map.get(e.v);
             //System.out.println(e);
-            //边的两个顶点都未出现在其他集合中
             if (setU == null && setV == null) {
                 Set<Integer> set = new HashSet<Integer>();
                 set.add(e.u);
                 set.add(e.v);
                 map.put(e.u, set);
                 map.put(e.v, set);
-            }//有一个顶点在其他集合中，一个不在，将不在的那个顶点集合合并进去
+            }
             else if (setU == null && setV != null) {
                 map.put(e.u, setV);
                 setV.add(e.u);
             } else if (setU != null && setV == null) {
                 map.put(e.v, setU);
                 setU.add(e.v);
-            }//分别在不同的集合中，合并两个集合
+            }
             else if (setU != setV) {
                 for (int v : setV) {
                     map.put(v, setU);
@@ -89,7 +86,7 @@ public class KruskalMST {
                 }
 
                 setU.addAll(setV);
-            }//两个顶点在同一个结合中，会出现环路，舍弃
+            }
             else {
                 continue;
             }
@@ -107,7 +104,6 @@ public class KruskalMST {
     }
 
     /**
-     * 生成边的排序好的队列
      *
      * @param graph
      * @return
